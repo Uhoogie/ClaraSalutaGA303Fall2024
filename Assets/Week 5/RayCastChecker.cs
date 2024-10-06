a69464f94a55085f6c67374f1f638ccc8529c7e6
Uhoogie <clarasaluta@lcad.edu>
2024-10-05 21:50:44 -0700

    wdasdw

--- Assets/Week 5/RayCastChecker.cs
+++ Assets/Week 5/RayCastChecker.cs
@@ -21,19 +21,20 @@ public class RayCastChecker : MonoBehaviour
                 if (hit.collider.CompareTag("Door")) //check for door tag
                 {
                   DoorScript doorScript = hit.collider.GetComponent<DoorScript>(); //in game checks for script and later can use the info so I can not have a million door thingys
-                  //(This is a note for future me btw since I will ref this in the future) I didn't do it with the keys me because I am disabling the keys and using trigger not ray casting to get them so it wouldn't work.
+                  //(This is a note for future me btw since I will ref this in the future cause I will forget) I didn't do it with the keys me because I am disabling the keys and using trigger not ray casting to get them so it wouldn't work.
+                 
                     {
 
                         if (doorScript.redDoor && KeycheckRed.gameObject.activeSelf == false) //checks the doors color and if you've collected the proper keys
                         {
-                            Debug.Log("Red door is open!");
+                            Debug.Log("Red door is open! Yipppeee!");
                             hit.collider.transform.Translate(Vector3.up * moveAmount);
                             Destroy(KeycheckRed);
                         }
 
                         else if (doorScript.yellowDoor && KeycheckYellow.gameObject.activeSelf == false)
                         {
-                            Debug.Log("Yellow door is open!");
+                            Debug.Log("Yellow door is open! Yahoo!");
                             hit.collider.transform.Translate(Vector3.up * moveAmount);
                             Destroy(KeycheckYellow);
                         }
@@ -41,13 +42,13 @@ public class RayCastChecker : MonoBehaviour
 
                         else if (doorScript.greenDoor && KeycheckGreen.gameObject.activeSelf == false)
                         {
-                            Debug.Log("Green door is open!");
+                            Debug.Log("Green door is open! Wooo!");
                             hit.collider.transform.Translate(Vector3.up * moveAmount);
                             Destroy(KeycheckRed);
                         }
                         else
                         {
-                            Debug.Log("Door is locked.");
+                            Debug.Log("Door is locked... sadge :(");
                         }
                     }
                 }
